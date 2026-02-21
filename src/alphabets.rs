47d20e6bfb3f50d2a709fecc4e5f927f66177f5c
Tom Amberson <53712733+TommyAmberson@users.noreply.github.com>
2026-02-20 18:21:10 -0600

    Add engram layout alphabet

--- src/alphabets.rs
+++ src/alphabets.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-const ALPHABETS: [(&'static str, &'static str); 22] = [
+const ALPHABETS: [(&'static str, &'static str); 26] = [
   ("numeric", "1234567890"),
   ("abcd", "abcd"),
   ("qwerty", "asdfqwerzxcvjklmiuopghtybn"),
@@ -23,6 +23,10 @@ const ALPHABETS: [(&'static str, &'static str); 22] = [
   ("colemak-homerow", "arstneiodh"),
   ("colemak-left-hand", "arstqwfpzxcv"),
   ("colemak-right-hand", "neioluymjhk"),
+  ("engram", "cieabyougxjkhtsnqldwvzrmfp"),
+  ("engram-homerow", "cieahtsnq"),
+  ("engram-left-hand", "cieabyougxjk"),
+  ("engram-right-hand", "htsnqldwvzrmfp"),
 ];
 
 pub struct Alphabet<'a> {
